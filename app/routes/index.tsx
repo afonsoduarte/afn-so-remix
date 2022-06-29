38d055465fd05f959e847a851904a9850d01f7be
Afonso Martins <a@afn.so>
2022-06-30 00:07:24 +0200

    First commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export default function Index() {
+  return (
+    <main>
+      <h1>La Afta Ltd</h1>
+      <p>
+        Serving the world wide web since 2001 For any enquiries, email
+        a[at]afn.so
+      </p>
+    </main>
+  )
+}
